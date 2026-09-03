bc3afad60c468183a7c6d1ce59354b76388a3556
neetyan56 <neetyan56@gmail.com>
2026-09-03 10:15:20 -0400

    publish to git hub

---
+++ script.js
@@ -0,0 +1,10 @@
+function updateClock() {
+    const now = new Date();
+
+    document.getElementById("clock").innerText =
+        now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
